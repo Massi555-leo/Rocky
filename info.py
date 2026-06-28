6fe58d769677162621470b69c2c41d4dea27fe80
Massi555-leo <manuleox444@gmail.com>
2026-06-28 11:42:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ ADMINS = [
 OWNER_USERNAME = environ.get(
     "OWNER_USERNAME", "IM_JISSHU"
 )  # without @ or https://t.me/
-USERNAME = environ.get("USERNAME", "")  # ADMIN USERNAME
+USERNAME = environ.get("USERNAME", "https://t.me/KANNADA_KID")  # ADMIN USERNAME
 
 # Database Channel
 CHANNELS = [
